5bb94a291c85499e369cc17485b564f754d1768d
Kevinhernandez97 <kmhs-18@hotmail.com>
2023-01-23 20:04:39 -0300

    first commit

---
+++ src/components/Square/index.jsx
@@ -0,0 +1,13 @@
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+  
+    const handleCliclk = () =>{
+      updateBoard(index)
+    }
+  
+    return (
+      <div className={className} onClick={handleCliclk}>
+        {children}
+      </div>
+    )
+  }
